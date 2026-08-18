e37a45c90475ddc89dc461978f5a2b0c1d77c8da
Al Cutter <al@google.com>
2026-08-18 10:56:29 +0100

    Add in-process mirror integration test (#1119)
    Adds an in-process end-to-end integration test for the mirror lifecycle, using POSIX as the storage.

--- integration/log/log_integration.go
+++ integration/log/log_integration.go
@@ -1,4 +1,4 @@
-// Copyright 2024 The Tessera authors. All Rights Reserved.
+// Copyright 2026 The Tessera Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,129 +12,73 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package integration_test contains some integration tests which are intended to
-// serve as a way of checking that example binary works as intended,
-// as well as providing a simple example of how to run and use it.
-package integration_test
+// Package log contains integration tests for a log.
+package log
 
 import (
 	"bytes"
 	"context"
-	"flag"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"sync"
 	"testing"
 	"time"
 
-	"log/slog"
-
-	f_note "github.com/transparency-dev/formats/note"
 	"github.com/transparency-dev/merkle/proof"
 	"github.com/transparency-dev/merkle/rfc6962"
 	"github.com/transparency-dev/tessera/api/layout"
 	"github.com/transparency-dev/tessera/client"
-
 	"golang.org/x/mod/sumdb/note"
 	"golang.org/x/sync/errgroup"
 )
 
-var (
-	runIntegrationTest = flag.Bool("run_integration_test", false, "If true, the integration tests in this package will not be skipped")
-	logURL             = flag.String("log_url", "http://localhost:2024", "Log storage read root URL, e.g. https://log.server/and/path/")
-	writeLogURL        = flag.String("write_log_url", "http://localhost:2024", "Log storage write root URL, e.g. https://log.server/and/path/")
-	logPublicKey       = flag.String("log_public_key", "", "The log's public key value for checkpoint note verification")
-	testEntrySize      = flag.Int("test_entry_size", 1024, "The number of entries to be tested in the live log integration")
+type LogIntegrationTestConfig struct {
+	LogReadTile        client.TileFetcherFunc
+	LogReadCP          client.CheckpointFetcherFunc
+	LogReadEntryBundle client.EntryBundleFetcherFunc
+	WriteLogURL        string
+	LogVerifier        note.Verifier
+	HTTPClient         *http.Client
 
-	noteVerifier note.Verifier
-
-	logReadBaseURL     *url.URL
-	logReadCP          client.CheckpointFetcherFunc
-	logReadTile        client.TileFetcherFunc
-	logReadEntryBundle client.EntryBundleFetcherFunc
-
-	hc = &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConns:        256,
-			MaxIdleConnsPerHost: 256,
-		},
-		Timeout: 60 * time.Second,
-	}
-)
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-
-	if !*runIntegrationTest {
-		slog.WarnContext(context.Background(), "example binary integration tests are skipped")
-		return
-	}
-
-	var err error
-	noteVerifier, err = f_note.NewVerifier(*logPublicKey)
-	if err != nil {
-		slog.ErrorContext(context.Background(), "Failed to create new verifier", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	logReadBaseURL, err = url.Parse(*logURL)
-	if err != nil {
-		slog.ErrorContext(context.Background(), "failed to parse logURL", slog.Any("error", err))
-		os.Exit(1)
-	}
-
-	switch logReadBaseURL.Scheme {
-	case "http", "https":
-		hf, err := client.NewHTTPFetcher(logReadBaseURL, nil)
-		if err != nil {
-			slog.ErrorContext(context.Background(), "NewHTTPFetcher", slog.Any("error", err))
-			os.Exit(1)
-		}
-		logReadCP = hf.ReadCheckpoint
-		logReadTile = hf.ReadTile
-		logReadEntryBundle = hf.ReadEntryBundle
-	case "file":
-		ff := client.FileFetcher{Root: logReadBaseURL.Path}
-		logReadCP = ff.ReadCheckpoint
-		logReadTile = ff.ReadTile
-		logReadEntryBundle = ff.ReadEntryBundle
-	default:
-		slog.ErrorContext(context.Background(), "unsupported url scheme", slog.String("scheme", logReadBaseURL.Scheme))
-		os.Exit(1)
-	}
-
-	os.Exit(m.Run())
+	EntrySize uint64
 }
 
-func TestLiveLogIntegration(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+// LogIntegrationTest performs the live log integration test.
+func LogIntegrationTest(t *testing.T, cfg LogIntegrationTestConfig) {
+	t.Helper()
+	if cfg.HTTPClient == nil {
+		cfg.HTTPClient = http.DefaultClient
+	}
+
+	ctx, cancel := context.WithTimeout(t.Context(), 5*time.Minute)
 	defer cancel()
 	var entryIndexMap sync.Map
 
 	// Step 1 - Get checkpoint initial size for increment validation.
-	lst, err := client.NewLogStateTracker(ctx, logReadTile, nil, noteVerifier, noteVerifier.Name(), client.UnilateralConsensus(logReadCP))
+	lst, err := client.NewLogStateTracker(ctx, cfg.LogReadTile, nil, cfg.LogVerifier, cfg.LogVerifier.Name(), client.UnilateralConsensus(cfg.LogReadCP))
 	if err != nil {
 		t.Fatalf("client.NewLogStateTracker: %v", err)
 	}
 	checkpointInitSize := lst.Latest().Size
 
 	// Step 2 - Add entries and get new checkpoints. The entry data comes from the int loop ranging from 0 to the test entry size - 1.
-	addEntriesURL, err := url.JoinPath(*writeLogURL, "add")
+	addEntriesURL, err := url.JoinPath(cfg.WriteLogURL, "add")
 	if err != nil {
 		t.Errorf("url.JoinPath: %v", err)
 	}
 	entryWriter := entryWriter{
+		hc:     cfg.HTTPClient,
 		addURL: addEntriesURL,
 	}
 	var miMu sync.Mutex
 	var maxIndex uint64
 	errG := errgroup.Group{}
-	for i := range *testEntrySize {
+	for i := range cfg.EntrySize {
 		errG.Go(func() error {
 			index, err := entryWriter.add(ctx, fmt.Appendf(nil, "%d", i))
 			if err != nil {
@@ -162,17 +106,17 @@ func TestLiveLogIntegration(t *testing.T) {
 	}
 
 	gotIncrease := lst.Latest().Size - checkpointInitSize
-	if gotIncrease < uint64(*testEntrySize) {
-		t.Logf("checkpoint size increase (%d) is < %d, entries may have been deduplicated.", gotIncrease, *testEntrySize)
+	if gotIncrease < cfg.EntrySize {
+		t.Logf("checkpoint size increase (%d) is < %d, entries may have been deduplicated.", gotIncrease, cfg.EntrySize)
 	}
 
 	// Step 3 - Loop through the entry data index map to verify leaves and inclusion proofs.
 	entryIndexMap.Range(func(k, v any) bool {
-		data := k.(int)
+		data := k.(uint64)
 		index := v.(uint64)
 
 		// Step 4.1 - Get entry bundles to read back what was written, check leaves are correct.
-		entryBundle, err := client.GetEntryBundle(ctx, logReadEntryBundle, index/layout.EntryBundleWidth, lst.Latest().Size)
+		entryBundle, err := client.GetEntryBundle(ctx, cfg.LogReadEntryBundle, index/layout.EntryBundleWidth, lst.Latest().Size)
 		if err != nil {
 			t.Fatalf("client.GetEntryBundle: %v", err)
 		}
@@ -183,7 +127,7 @@ func TestLiveLogIntegration(t *testing.T) {
 		}
 
 		// Step 4.2 - Test inclusion proofs.
-		pb, err := client.NewProofBuilder(ctx, lst.Latest().Size, logReadTile)
+		pb, err := client.NewProofBuilder(ctx, lst.Latest().Size, cfg.LogReadTile)
 		if err != nil {
 			t.Errorf("client.NewProofBuilder: %v", err)
 		}
@@ -201,6 +145,7 @@ func TestLiveLogIntegration(t *testing.T) {
 }
 
 type entryWriter struct {
+	hc     *http.Client
 	addURL string
 }
 
@@ -209,7 +154,7 @@ func (w *entryWriter) add(ctx context.Context, entry []byte) (uint64, error) {
 	if err != nil {
 		return 0, err
 	}
-	resp, err := hc.Do(req)
+	resp, err := w.hc.Do(req)
 	if err != nil {
 		return 0, err
 	}
