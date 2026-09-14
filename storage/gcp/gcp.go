db19d82d8aafd8321723a54752d007154b880581
Roger Ng <rogerng@google.com>
2026-09-14 16:35:56 +0000

    fix: correct range calculation in fetchLeafHashes by passing length instead of end index

--- storage/gcp/gcp.go
+++ storage/gcp/gcp.go
@@ -1555,6 +1555,7 @@ func (m *MigrationStorage) IntegratedSize(ctx context.Context) (uint64, error) {
 	return sz, err
 }
 
+// fetchLeafHashes returns the leaf hashes for the entries in the range [from, to).
 func (m *MigrationStorage) fetchLeafHashes(ctx context.Context, from, to, sourceSize uint64) ([][]byte, error) {
 	// TODO(al): Make this configurable.
 	const maxBundles = 100
@@ -1562,7 +1563,7 @@ func (m *MigrationStorage) fetchLeafHashes(ctx context.Context, from, to, source
 	toBeAdded := sync.Map{}
 	eg := errgroup.Group{}
 	n := 0
-	for ri := range layout.Range(from, to, sourceSize) {
+	for ri := range layout.Range(from, to-from, sourceSize) {
 		eg.Go(func() error {
 			b, err := m.logStore.getEntryBundle(ctx, ri.Index, ri.Partial)
 			if err != nil {
